fc033dc233ef524cadcb46fc7a79087ebcbc2316
Muhammad Alqi Fahrezi <mohammedalqifahrezi@gmail.com>
2026-06-22 15:49:43 +0700

    Update vuln.py

--- vuln.py
+++ vuln.py
@@ -7,4 +7,6 @@ def get_user(uid):
     db = sqlite3.connect("app.db")
     db.execute("SELECT * FROM users WHERE id = " + uid)
 
+#API KEY
 API_KEY = "cor_O2CjoeUATapgi1aLMCjIqNZWb9BxJTMxtmUxf15jtv4"
+
