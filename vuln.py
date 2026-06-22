26b7c6201afa1f3a56e624193d7742bccdd88968
Muhammad Alqi Fahrezi <mohammedalqifahrezi@gmail.com>
2026-06-22 15:56:51 +0700

    Delete vuln.py

--- vuln.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-# SECURE: Using subprocess prevents argument injection
-safe_folder = input("Enter the folder name to list: ")
-# Pass arguments as a list to avoid shell interpretation
-subprocess.run(["ls", "-la", safe_folder])
